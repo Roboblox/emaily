36487004fe5b06d4d27e539d50ca36bfc5aed7bc
neojohnny <schulerjohnthomas@gmail.com>
2018-06-16 19:06:33 -0700

    test

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const express = require("express");
-/*const mongoose = require("mongoose");
-const cookieSession = require("cookie-session");
+//const mongoose = require("mongoose");
+//const cookieSession = require("cookie-session");
 const passport = require("passport");
-const bodyParser = require("body-parser");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+//const bodyParser = require("body-parser");
 const keys = require("./config/keys");
-require("./models/User");
-require("./models/Survey");
-require("./services/passport");*/
+//require("./models/User");
+//require("./models/Survey");
+//require("./services/passport");*/
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ bye: "there" });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      cleintSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log("accessToken", accessToken);
+      console.log("refresh token", refreshToken);
+      console.log("profile", profile);
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+
+app.get("auth/google/callback", passport.authenticate("google"));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
